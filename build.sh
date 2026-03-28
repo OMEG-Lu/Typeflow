6517bfc5708afb2552de14e4049ffd00e8716b30
Lu <yufenglu@Lus-MacBook-Pro.local>
2026-03-28 03:25:42 -0400

    Disable codesigning in CI release build

--- build.sh
+++ build.sh
@@ -6,7 +6,15 @@ rm -rf /tmp/hallelujah
 
 xcodebuild clean -workspace hallelujah.xcworkspace/ -scheme hallelujah
 
-xcodebuild -workspace hallelujah.xcworkspace/ -scheme hallelujah -destination "generic/platform=macOS,name=Any Mac" -configuration "Release" CONFIGURATION_BUILD_DIR=/tmp/hallelujah/build/release BUILD_LIBRARY_FOR_DISTRIBUTION=YES
-
+xcodebuild \
+  CODE_SIGN_IDENTITY="" \
+  CODE_SIGNING_REQUIRED=NO \
+  CODE_SIGNING_ALLOWED=NO \
+  -workspace hallelujah.xcworkspace/ \
+  -scheme hallelujah \
+  -destination "generic/platform=macOS,name=Any Mac" \
+  -configuration "Release" \
+  CONFIGURATION_BUILD_DIR=/tmp/hallelujah/build/release \
+  BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 
